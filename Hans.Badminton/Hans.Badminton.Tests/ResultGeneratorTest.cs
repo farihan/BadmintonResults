388daac9ea0066b244d8d800cff7002aef6bb03f
Muhamad Farihan <farihan@gmail.com>
2017-02-13 01:32:28 +0800

    modify tests to reflect on changes

--- Hans.Badminton/Hans.Badminton.Tests/ResultGeneratorTest.cs
+++ Hans.Badminton/Hans.Badminton.Tests/ResultGeneratorTest.cs
@@ -7,12 +7,14 @@ namespace Hans.Badminton.Tests
     [TestClass]
     public class ResultGeneratorTest
     {
+        string text1 = "pokd jack 21 kaa hans 11\r\npul oden 21 pokd hans 17\r\nkaa ant 22 oden pokd 24";
+        string text2 = "pokd jack 21 kaa hans 11\r\npul oden 21 pokd hans 17\r\npul ant 21 pokd oden 19\r\noden ant 21 kaa hans 15\r\npokd pul 21 pul hans 19\r\noden kaa 21 pokd pul 17\r\noden ant 21 pokd pul 15\r\nkaa hans 21 oden ant 7\r\noden pokd 21 pul hans 19\r\nkaa ant 22 oden pokd 20";
+
         [TestMethod]
         public void CanPopulateRawResults()
         {
-            var file = @"C:\Downloads\Workspaces\Hans.Badminton\Hans.Badminton.Tests\bin\Debug\TestData_1.txt";
-
-            var generator = new ResultGenerator(file, string.Empty);
+            //var file = @"C:\Downloads\Workspaces\Hans.Badminton\Hans.Badminton.Tests\bin\Debug\TestData_1.txt";
+            var generator = new ResultGenerator(string.Empty, text1);
             generator.PopulateRawResults();
 
             Assert.AreEqual(3, generator.RawResults.Count);
@@ -27,23 +29,22 @@ namespace Hans.Badminton.Tests
         [TestMethod]
         public void CanPopulateLeagueRankingResults()
         {
-            var file = @"C:\Downloads\Workspaces\Hans.Badminton\Hans.Badminton.Tests\bin\Debug\TestData_1.txt";
-
-            var generator = new ResultGenerator(file, string.Empty);
+            //var file = @"C:\Downloads\Workspaces\Hans.Badminton\Hans.Badminton.Tests\bin\Debug\TestData_1.txt";
+            var generator = new ResultGenerator(string.Empty, text1);
             generator.PopulateRawResults();
             generator.PopulateLeagueRankingResults();
 
             Assert.AreEqual(3, generator.RawResults.Count);
             Assert.AreEqual(3, generator.LeagueRankings.Count);
 
-            Assert.AreEqual("POKD", generator.LeagueRankings[0].Winner1);
-            Assert.AreEqual("KAA", generator.LeagueRankings[0].Loser1);
+            Assert.AreEqual("JACK", generator.LeagueRankings[0].Winner1);
+            Assert.AreEqual("HANS", generator.LeagueRankings[0].Loser1);
             Assert.AreEqual(21, generator.LeagueRankings[0].WinnerPoint);
             Assert.AreEqual(11, generator.LeagueRankings[0].LoserPoint);
             Assert.AreEqual(10, generator.LeagueRankings[0].Different);
 
             Assert.AreEqual("POKD", generator.LeagueRankings[2].Winner2);
-            Assert.AreEqual("ANT", generator.LeagueRankings[2].Loser2);
+            Assert.AreEqual("KAA", generator.LeagueRankings[2].Loser2);
             Assert.AreEqual(24, generator.LeagueRankings[2].WinnerPoint);
             Assert.AreEqual(22, generator.LeagueRankings[2].LoserPoint);
             Assert.AreEqual(2, generator.LeagueRankings[2].Different);
@@ -52,9 +53,8 @@ namespace Hans.Badminton.Tests
         [TestMethod]
         public void CanPopulateRawPlayers()
         {
-            var file = @"C:\Downloads\Workspaces\Hans.Badminton\Hans.Badminton.Tests\bin\Debug\TestData_1.txt";
-
-            var generator = new ResultGenerator(file, string.Empty);
+            //var file = @"C:\Downloads\Workspaces\Hans.Badminton\Hans.Badminton.Tests\bin\Debug\TestData_1.txt";
+            var generator = new ResultGenerator(string.Empty, text1);
             generator.PopulateRawResults();
             generator.PopulateLeagueRankingResults();
             generator.PopulateRawPlayers();
@@ -63,11 +63,11 @@ namespace Hans.Badminton.Tests
             Assert.AreEqual(3, generator.LeagueRankings.Count);
             Assert.AreEqual(12, generator.RawPlayers.Count);
 
-            Assert.AreEqual("POKD", generator.RawPlayers[0].Initial);
+            Assert.AreEqual("JACK", generator.RawPlayers[0].Initial);
             Assert.AreEqual(21, generator.RawPlayers[0].Point);
             Assert.AreEqual(true, generator.RawPlayers[0].IsWinner);
 
-            Assert.AreEqual("KAA", generator.RawPlayers[2].Initial);
+            Assert.AreEqual("HANS", generator.RawPlayers[2].Initial);
             Assert.AreEqual(11, generator.RawPlayers[2].Point);
             Assert.AreEqual(false, generator.RawPlayers[2].IsWinner);
 
@@ -78,10 +78,9 @@ namespace Hans.Badminton.Tests
 
         [TestMethod]
         public void CanPopulatePlayerRankings()
-        {
-            var file = @"C:\Downloads\Workspaces\Hans.Badminton\Hans.Badminton.Tests\bin\Debug\TestData_2.txt";
-
-            var generator = new ResultGenerator(file, string.Empty);
+        { 
+            //var file = @"C:\Downloads\Workspaces\Hans.Badminton\Hans.Badminton.Tests\bin\Debug\TestData_1.txt";
+            var generator = new ResultGenerator(string.Empty, text2);
             generator.PopulateRawResults();
             generator.PopulateLeagueRankingResults();
             generator.PopulateRawPlayers();
